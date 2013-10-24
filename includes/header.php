a047c2b656c667b228f09f0a36757c88c9e20c48
Melissa <wildlotus@mail.com>
2013-10-23 22:40:52 -0400

    modified search header

--- includes/header.php
+++ includes/header.php
@@ -13,8 +13,9 @@
 					</div>
 					<div class="threecol">	
 					<div class="dark">
-						<form method="get" action="post" id="search" class="dark">
-							<input name="q" type="text" size="40" placeholder="Search..." >
+						 <form action="search.php" method="post" class="search">
+                        <input type="text" name="term" value="Search" onclick="this.value = '';" onkeydown="this.style.color = '#4b4b4b';">
+                         <input id="search-bar" type="submit" name="search" value=""/>
 						</form>
 					</div>
 					</div>
